2128ef0e03a517f223fa4399e25f68582f806d3f
Marcos Rodrigues <marcosaureliorod@gmail.com>
2011-06-28 20:34:46 -0300

    add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finance::Application.config.secret_token = '31e6d77d9e0e6cfa4dd44e9517d98c67ddd21ca2a49ccc96f1b2b51ba53c2af561906078aa1c6decd1d321f616cfe94e0272bb4fa64e1e9ef5a732e3c2410d9a'
