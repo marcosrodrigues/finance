8e24a124ae627521eb80b58239c438904b07c5e1
Marcos Rodrigues <marcosaureliorod@gmail.com>
2011-07-02 13:42:34 -0300

    proximas contas a pagar e receber na home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
 class HomeController < ApplicationController
   
+  before_filter :load_next_pays
+  before_filter :load_next_receivables
+  
   def index
     
   end  
+  
+private
+  def load_next_pays
+    @next_pays = Pay.find(:all, 
+      :conditions => ["maturity between ? and ?", Time.now, Time.now.next_month])
+  end
+  
+  def load_next_receivables
+    @next_receivables = Receivable.find(:all, 
+      :conditions => ["maturity between ? and ?", Time.now, Time.now.next_month])
+  end
   
 end
